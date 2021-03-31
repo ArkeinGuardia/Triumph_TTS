3bd1a9b493005853431af9dcd0c41e5a76ace5b6
Marc Pawlowsky <marcpawl@gmail.com>
2021-03-30 20:18:05 -0400

    Plain base can have nil or 0 models

--- unittests/test_spawnarmy.lua
+++ unittests/test_spawnarmy.lua
@@ -5,7 +5,7 @@ function test_get_a_model_definition_returns_plain_base()
     local base_def = g_base_definitions[g_str_5fb1ba44e1af06001770feb5]
     local model_def = get_a_model_definition(base_def)
     lu.assertEquals(model_def.base, 'tile_plain_40x20_Warband' )
-    lu.assertEquals(n_models, 0 )
+    lu.assertEquals(n_models, nil )
 end
 
 os.exit( lu.LuaUnit.run() )
