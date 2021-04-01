642e7d4af6f686ff633fab183fa256a27e4de67c
Marc Pawlowsky <marcpawl@gmail.com>
2021-03-31 21:36:42 -0400

    Add sanity check for figures
    A few more sanity checks for fixed models also.

--- unittests/test_models.lua
+++ unittests/test_models.lua
@@ -1,6 +1,27 @@
 lu = require('externals/luaunit/luaunit')
 require('flatten')
 
+function check_figure(figure)
+  lu.assertEquals(type(figure.height_correction), 'number')
+  lu.assertEquals(type(figure.scale), 'number')
+  lu.assertEquals(type(figure.rotation), 'number')
+  if figure.description ~= nil then
+    lu.assertEquals(type(figure.description), 'string')
+  end
+  if figure.author ~= nil then
+    lu.assertEquals(type(figure.author), 'string')
+  end
+  lu.assertFalse(figure.mesh == nil)
+  lu.assertTrue(tlen(figure.mesh) > 0)
+  for _,mesh in pairs(figure.mesh) do
+    lu.assertEquals(type(mesh), "string")
+  end
+  lu.assertFalse(figure.player_red_tex == nil)
+  lu.assertEquals(type(figure.player_red_tex), "string")
+  lu.assertFalse(figure.player_blue_tex == nil)
+  lu.assertEquals(type(figure.player_blue_tex), "string")
+end
+
 function test_models_have_base()
   for id,models in pairs(g_models) do
     for i,model in pairs(models) do
@@ -25,6 +46,22 @@ function test_models_with_fixed_figures_have_right_number()
         local actual = tlen(model['fixed_models'])
         if n_models ~= actual then
           print("g_models['" .. id .. "'] has wrong number of models.")
+        end
+        for j,figure in pairs(model['fixed_models']) do
+          if "string" == type(figure) then
+            if nil == _G[figure] then
+              print("No variable ", figure)
+              lu.assertTrue(false)
+            end
+            if "table" ~= type(_G[figure]) then
+              print(figure, " figure should be table")
+              lu.assertTrue(false)
+            end
+            check_figure(_G[figure])
+          else
+            print("g_models['" .. id .. "'][" .. tostring(i) .. "][" .. tostring(j) .. "] should be string.")
+            lu.assertTrue(false)
+          end
         end
         lu.assertEquals(n_models, actual)
       end
