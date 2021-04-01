67c673aa97d0929cfe19f979402186f4fd337121
Marc Pawlowsky <marcpawl@gmail.com>
2021-04-01 17:31:16 -0400

    Add option to deploy plain bases.
    Instead of having each army list duplicated, an option is
    given to prefer using plain bases.

--- unittests/test_spawnarmy.lua
+++ unittests/test_spawnarmy.lua
@@ -14,6 +14,22 @@ function test_get_a_model_definition_returns_model_base()
     lu.assertEquals(model_def.base, 'tile_grass_40x40')
 end
 
+function test_get_a_model_definition_returns_plain_base_if_preferred()
+    -- Setup
+    local old = g_use_plain_bases
+    g_use_plain_bases = true
+
+    -- Exercise
+    local base_def = g_base_definitions[g_str_5fb1ba34e1af06001770e186]
+    local model_def = get_a_model_definition(base_def)
+
+    -- Validate
+    lu.assertEquals(model_def.base, 'tile_plain_40x40_Artillery')
+
+    -- Cleanup
+    g_use_plain_bases = old
+end
+
 -- Random model iterator will always return a non-nil figure to add to a base.
 function test_random_model_iterator()
     local model_def = g_models[g_str_5fb1ba26e1af06001770c82a]
