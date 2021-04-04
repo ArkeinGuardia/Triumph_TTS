848bdaa6174e10c43eaf983514cc7657dca04f8c
Marc Pawlowsky <marcpawl@gmail.com>
2021-04-04 13:50:02 -0400

    Add troop type to base definition
    No more need to parse the name to get the troop type.

--- fake_meshwesh/tts_army.py
+++ fake_meshwesh/tts_army.py
@@ -37,11 +37,11 @@ def troop_type_to_name(troop_type) :
     return "Artillery"
   if troop_type == "JCV" :
     return "Javelin Cavalry"
-  if troop_type == "SPR" :
+  if troop_type == "SPR" or troop_type == "Spear" or troop_type == "Spears":
     return "Spears"
   if troop_type == "ELE" :
     return "Elephants"
-  if troop_type == "WRR" :
+  if troop_type == "WRR" or troop_type == "Warrior" or troop_type == "Warriors":
     return "Warriors"
   if troop_type == "BTX" :
     return "Battle Taxi"
@@ -49,9 +49,9 @@ def troop_type_to_name(troop_type) :
     return "Bad Horse"
   if troop_type == "WBD" :
     return "Warband"
-  if troop_type == "ARC" :
+  if troop_type == "ARC" or troop_type == "Archer" or troop_type == "Archers":
     return "Archers"
-  if troop_type == "RDR" :
+  if troop_type == "RDR" or troop_type == "Raider" or troop_type == "Raiders":
     return "Raiders"
   if troop_type == "BLV" :
     return "Bow Levy"
@@ -63,13 +63,13 @@ def troop_type_to_name(troop_type) :
     return "Skirmishers"
   if troop_type == "CHT" :
     return "Chariots"
-  if troop_type == "LFT" :
+  if troop_type == "LFT" or troop_type == "Light Foot" :
     return "Light Foot"
-  if troop_type == "HFT" :
+  if troop_type == "HFT" or troop_type == "Heavy Foot" :
     return "Heavy Foot"
-  if troop_type == "EFT" :
+  if troop_type == "EFT" or troop_type == "Elite Foot" :
     return "Elite Foot"
-  if troop_type == "PIK" :
+  if troop_type == "PIK" or troop_type == "Pike" or troop_type == "Pikes":
     return "Pikes"
   if troop_type == "LSP" :
     return 'Light Spear'
@@ -223,7 +223,6 @@ def create_base_definition(troop_option, troop_entry) :
   troop_type = troop_entry['troopTypeCode']
   name = troop_type_to_name(troop_type)
 
-  name = troop_type_to_name(troop_type)
   base_definition = {
     'id': id, 'name':name, 'troop_type':troop_type, 
     'min':min, 'max':max, 
@@ -252,6 +251,8 @@ def write_base_definition(file, base_definition) :
     file.write("  dismount_as=%s,\n" % (base_definition['dismount_as']))
   if 'general' in base_definition  and base_definition['general'] == True :
     file.write("  general=true,\n")
+  troop_type_name = troop_type_to_name( base_definition['troop_type'])
+  file.write("  troop_type=\"%s\",\n" %(troop_type_name))
   file.write("}\n")
 
 def get_general_troop_type_codes(army) :
