7c9e8c23221a844f3a5bad9bcf7cfca59d9c4187
Marc Pawlowsky <marcpawl@gmail.com>
2021-04-26 13:14:20 -0400

    Remove snapping rule right_to_wwg_back
    No longer used

--- unittests/test_logic.lua
+++ unittests/test_logic.lua
@@ -561,30 +561,6 @@ function test_snap_to_base_left_to_wwg_front()
   lu.assertPointAlmostEquals(transform_actual.corners.topleft, transform_resting.corners.topleft)
 end
 
-function test_right_to_wwg_back()
-  local resting_base = build_base("base WWg # 20", 'tile_plain_40x40_War_Wagon')
-  local transform_resting = calculate_transform(resting_base)
-
-  local moving_base = build_base("base Bw # 19")
-  local transform_moving = calculate_transform(moving_base)
-  local delta_x = transform_resting.corners.botleft.x - transform_moving.corners.topright.x
-  local delta_z = transform_resting.corners.botleft.z - transform_moving.corners.topright.z
-  moving_base.position['x'] = moving_base.position['x'] + delta_x
-  moving_base.position['z'] = moving_base.position['z'] + delta_z
-  moving_base.setRotation({0, -90, 0})
-  jiggle(moving_base)
-  transform_moving = calculate_transform(moving_base)
-
-  -- Exercise
-  snap_to_base(moving_base, transform_moving, resting_base, transform_resting, 'right_to_wwg_back')
-
-  -- Verify
-  local transform_actual = calculate_transform(moving_base)
-  local actual_rotation = transform_actual.rotation
-  lu.assertAlmostEquals(actual_rotation, normalize_radians(transform_resting.rotation+(math.pi*0.5)), 0.01)
-  lu.assertPointAlmostEquals(transform_actual.corners.topright, transform_resting.corners.botleft)
-end
-
 
 function test_transform_to_shape()
   local base = build_base("base 4Bw # 16")
