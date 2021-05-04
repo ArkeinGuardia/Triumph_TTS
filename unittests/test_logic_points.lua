cc9f924bf07bf7b80f5059b7b5baf424fcde9ad8
Marc Pawlowsky <marcpawl@gmail.com>
2021-05-04 18:53:45 -0400

    Army builder points for battle cards up to plaustrella

--- unittests/test_logic_points.lua
+++ unittests/test_logic_points.lua
@@ -1,13 +1,6 @@
 lu = require('externals/luaunit/luaunit')
-armies = {}
-require('scripts/utilities_lua')
-require('scripts/utilities')
-require('scripts/logic_base_obj')
-require('scripts/logic_dead')
-require('scripts/logic_spawn_army')
-require('scripts/data/data_troops')
+require('flatten')
 
-g_decorations = {}
 
 function test_all_bases_are_counted()
   local base1_obj = { getName = function() return "base Archers #48" end, getGUID = function() return "ABCDE" end }
@@ -139,6 +132,75 @@ function test_total_points_are_rounded_up_for_casualties()
 
   -- cleanup
   get_base_definition_from_base_obj = old_fn
+end
+
+function test_get_army_builder_points_for_base_defintions_uses_points_from_tool_tip()
+  local def = g_base_definitions[g_str_5fb1ba1ee1af06001770bd94] -- a rabble
+  local actual = get_army_builder_points_for_base_definitions({def})
+  lu.assertEquals(actual, 2)
+end
+
+function test_get_army_builder_points_for_base_defintions_does_not_use_victory_points()
+  local def = g_base_definitions[g_str_5fb1b9d8e1af0600177092b3_camp]
+  local actual = get_army_builder_points_for_base_definitions({def})
+  lu.assertEquals(actual, 0)
+end
+
+
+function test_get_army_builder_points_for_base_defintion_uses_max_for_dismounting()
+  local def = g_base_definitions[g_str_5fb1ba2fe1af06001770d99d_general_mounted]
+  local actual = get_army_builder_points_for_base_definition(def)
+  lu.assertEquals(actual, 4)
+end
+
+
+function test_get_army_builder_points_for_base_defintion_adds_1_point_for_any_deployment_dismounting()
+  local def1 = g_base_definitions[g_str_5fb1ba2fe1af06001770d99d_general_mounted]
+  local def2 = g_base_definitions[g_str_5fb1ba2fe1af06001770d99d_mounted]
+  local actual = get_army_builder_points_for_base_definitions({def1, def2})
+  lu.assertEquals(actual, 9)
+end
+
+function test_get_army_builder_points_for_base_defintion_adds_2_points_for_any_mid_battle_dismounting()
+  local def1 = g_base_definitions[g_str_5fb1ba31e1af06001770dbd0_general_mounted]
+  local def2 = g_base_definitions[g_str_5fb1ba31e1af06001770dbd0_general_mounted]
+  local actual = get_army_builder_points_for_base_definitions({def1, def2})
+  lu.assertEquals(actual, 10)
+end
+
+function test_get_army_builder_points_for_charging_camelry_1_point_less_than_normal()
+  local def = g_base_definitions[g_str_5fb1ba32e1af06001770de42_charging_camelry]
+  local actual = get_army_builder_points_for_base_definition(def)
+  -- knights normally cost 4 points
+  lu.assertEquals(actual, 3)
+end
+
+function test_get_army_builder_points_for_armored_camelry_1_point_less_than_normal()
+  local def = g_base_definitions[g_str_5fb1ba2ae1af06001770cf87_armored_camelry]
+  local actual = get_army_builder_points_for_base_definition(def)
+  -- Cataphracts normally cost 4 points
+  lu.assertEquals(actual, 3)
+end
+
+function test_get_army_builder_points_for_1_mobile_infantry()
+  local def = g_base_definitions[g_str_5fb1ba22e1af06001770c30b_mounted_mobile_infantry]
+  local actual = get_army_builder_points_for_base_definitions({def})
+  -- same costs as normal
+  lu.assertEquals(actual, 2)
+end
+
+function test_get_army_builder_points_for_3_mobile_infantry()
+  local def = g_base_definitions[g_str_5fb1ba22e1af06001770c30b_mounted_mobile_infantry]
+  local actual = get_army_builder_points_for_base_definitions({def,def,def})
+  -- 1 extra for more than 1 mobile infranty
+  lu.assertEquals(actual, 7)
+end
+
+function test_get_army_builder_points_plaustrella_one_point_per_stand()
+  local def= g_base_definitions[g_str_5fb1ba35e1af06001770e449_plaustrella]
+  local actual = get_army_builder_points_for_base_definitions({def})
+  -- heavy foot are normally 3
+  lu.assertEquals(actual, 4)
 end
 
 os.exit( lu.LuaUnit.run() )
